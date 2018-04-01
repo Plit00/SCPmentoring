0598ecbcbcf845f90a5e9b93c436861478f2a9f6
diededith404 <eovyxla14@gmail.com>
2018-04-01 13:20:12 +0900

    Create switch)9498.c

---
+++ switch)9498.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int i = a;
+	int score;
+
+	switch(ninput)
+	{
+
+		case 1 : 
+		(score >= 90)
+        cout << 'A' << "\n";
+
+    	case 2:
+    	(score >= 80);
+         cout << 'B' << "\n";
+
+    	case 3:
+        (score >= 70);
+    	cout << 'C' << "\n";
+    	
+    	case 4:
+    	(score >= 60);
+    	cout << 'D' << "\n";
+
+    	default:
+    	 case 5:
+    	cout << 'F' << "\n";
+
+        return 0;
+
+	}
